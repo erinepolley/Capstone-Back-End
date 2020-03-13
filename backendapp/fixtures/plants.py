4ce23c53290ff3ac8722bca542dd4117d6a8cf6a
Erin Polley <erinepolley@gmail.com>
2020-03-12 21:45:21 -0500

    started fixtures accidentally

---
+++ backendapp/fixtures/plants.py
@@ -0,0 +1,14 @@
+[
+    {
+      "model": "hrapp.plant",
+      "pk": 1,
+      "fields": {
+        "name": "Steve",
+        "description": "Brownlee",
+        "img_url": "2014-04-05",
+        "days": true,
+        "weeks": 1,
+        "reminder_time": 1,
+        "user": 1 
+      }
+    },
