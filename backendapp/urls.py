4ecac266b6b8b38f4477d69fd4a9852949550be3
Erin Polley <erinepolley@gmail.com>
2020-03-16 11:53:30 -0500

    can see form without plant_type dropdown

--- backendapp/urls.py
+++ backendapp/urls.py
@@ -1,14 +1,14 @@
 from django.urls import include, path
 from .views import *
 
-app_name = "libraryapp"
+app_name = "backendapp"
 
 urlpatterns = [
     path('', plant_list, name='home'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('logout/', logout_user, name='logout'),
     path('plants/<int:plant_id>', plant_details, name='plant'),
-    # path('plant/form', plant_form, name='plant_form'),
-    # path('plants/<int:plant_id>/form/', plant_edit_form, name='book_edit_form'),
+    path('plant/form', plant_form, name='plant_form'),
+    path('plants/<int:plant_id>/form/', plant_edit_form, name='plant_edit_form'),
 
 ]
--- backendapp/views/plants/form.py
+++ backendapp/views/plants/form.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from backendapp.models import Plant
+from backendapp.models import PlantType
 from .detail import get_plant
 
 
@@ -8,7 +9,7 @@ from .detail import get_plant
 def plant_form(request):
     if request.method == 'GET':
         plant_type_list = PlantType.objects.all()
-        template = 'templates/plant_form.html'
+        template = 'plant_form.html'
         context = {
             'plant_types': plant_type_list
         }
@@ -20,7 +21,7 @@ def plant_edit_form(request, plant_id):
     if request.method == 'GET':
         plant = get_plant(plant_id)
 
-        template = 'templates/plant_form.html'
+        template = 'plant_form.html'
         context = {
             'plant': plant
         }
