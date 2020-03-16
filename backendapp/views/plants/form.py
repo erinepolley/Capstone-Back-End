353b88f2e71198daaccf82f90beb58503a588f72
Erin Polley <erinepolley@gmail.com>
2020-03-16 11:24:55 -0500

    added plant type to the get form method in views/plants/form :q :qa quit laskjd

--- backendapp/views/plants/form.py
+++ backendapp/views/plants/form.py
@@ -7,11 +7,12 @@ from .detail import get_plant
 @login_required
 def plant_form(request):
     if request.method == 'GET':
-        libraries = get_libraries()
-        template = 'templates/form.html'
-
-
-        return render(request, template)
+        plant_type_list = PlantType.objects.all()
+        template = 'templates/plant_form.html'
+        context = {
+            'plant_types' = plant_type_list
+        }
+        return render(request, template, context)
       
 @login_required
 def plant_edit_form(request, plant_id):
@@ -19,7 +20,7 @@ def plant_edit_form(request, plant_id):
     if request.method == 'GET':
         plant = get_plant(plant_id)
 
-        template = 'templates/form.html'
+        template = 'templates/plant_form.html'
         context = {
             'plant': plant
         }
