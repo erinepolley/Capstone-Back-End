69de75e26a894a42b9d37fac7f05168c110ab732
Erin Polley <erinepolley@gmail.com>
2020-03-21 10:12:50 -0500

    wrote template for thirsty plants with a conditional for when there aren't thirsty plants

--- backendapp/views/plants/watering.py
+++ backendapp/views/plants/watering.py
@@ -15,7 +15,7 @@ def watering_list(request):
 		dateTimeObj = datetime.now(timezone.utc)
 		# print("Today's full date time object", dateTimeObj)
 		justTodaysDate=dateTimeObj.date()
-		# print("Just today's date", justTodaysDate)
+		print("Just today's date", justTodaysDate)
 		#Let's get all the plants for this user.
 		user_plants = Plant.objects.filter(user=current_user)
 		#Empty array to store all the plants that need to be watered. To be explained below...
@@ -27,10 +27,10 @@ def watering_list(request):
 			# print("MOST RECENT WATERING", most_recent_watering_object)
 			#Then, let's isolate the date of the most recent watering from the datetime.
 			justPlantWateringDate = most_recent_watering_object.time.date() 
-			# print("Just the plant's reminder date?", justPlantWateringDate)
+			print("Just the plant's reminder date", justPlantWateringDate)
 			#Doing some simple math to determine the date that the plant needs to be watered.
 			dateThatPlantNeedsToBeWatered = justPlantWateringDate + timedelta(weeks=plant.weeks, days=plant.days)
-			# print("dateThatPlantNeedsToBeWatered", dateThatPlantNeedsToBeWatered)
+			print("dateThatPlantNeedsToBeWatered", dateThatPlantNeedsToBeWatered)
 			#If today's date is past the date that the plant needs to be watered, add the plant to the thirsty plant list.
 			if dateThatPlantNeedsToBeWatered <= justTodaysDate:
 				listOfThirstyPlants.append(plant)
@@ -39,8 +39,9 @@ def watering_list(request):
 			else:
 				print(f'{plant.name} the {plant.description} is fine.')
 		#Let's send these thirsty plants to a template to let the user know!
-		template = 'watering_list.html'
+		template = 'plant_watering.html'
 		context = {
-			'thirsty_plants': listOfThirstyPlants
+			'thirsty_plants': listOfThirstyPlants,
+			'current_user': current_user
 		}
 		return render(request, template, context)
