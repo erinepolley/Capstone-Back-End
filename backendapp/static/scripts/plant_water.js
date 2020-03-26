7cafded6de6b252bc2159ab6e35a07a0ad65d12d
Erin Polley <erinepolley@gmail.com>
2020-03-26 11:28:34 -0500

    changed textarea to text. Added a new background

--- backendapp/static/scripts/plant_water.js
+++ backendapp/static/scripts/plant_water.js
@@ -1,26 +1,9 @@
 console.log("Hullo!")
 
 document.querySelector("body").addEventListener("click", (evt) => {
-  // alert("The water button was just clicked.")
   if (evt.target.id.startsWith("water")) {
         const name = evt.target.id.split("--")[1]
         alert(`${name} has been watered!`)
     }
 })
-
-// let mainNav = document.getElementById('menu-items');
-// let hamburgerContainer = document.getElementById('hamburger-container');
-
-// hamburgerContainer.addEventListener('click', () => {
-//     mainNav.classList.toggle('active');
-// });
-
-const responsiveNav = () => {
-    let menu = document.getElementById("menu-items");
-    if (menu.className === "topnav") {
-      menu.className += " responsive";
-    } else {
-      menu.className = "topnav";
-    }
-  }
 
