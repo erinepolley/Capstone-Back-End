89a7a942e2edda3873b0a52286f9bd6623d8fccd
Erin Polley <erinepolley@gmail.com>
2020-04-13 22:00:22 -0500

    found working modal code from W3 schools, going back and customizing it

--- backendapp/static/scripts/plant_water.js
+++ backendapp/static/scripts/plant_water.js
@@ -7,3 +7,29 @@ document.querySelector("body").addEventListener("click", (evt) => {
     }
 })
 
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("plant-guide-button");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = () => {
+  event.preventDefault()
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = () => {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
